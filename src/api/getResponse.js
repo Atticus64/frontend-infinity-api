4d1b14cae1b954cfe456db7dfa395fc77adfc725
Jona <jonathanelian64@gmail.com>
2022-12-20 22:30:41 -0700

    Add prism dependencie and remove unused deps and change preview response

--- src/api/getResponse.js
+++ src/api/getResponse.js
@@ -1,48 +1,41 @@
 const apiUrl = 'https://the-infinity-train-api.deno.dev/api'
-
-import JSONFormatter from 'json-formatter-js'
+import Prism from 'prismjs'
 
 const $ = (q) => document.querySelector(q)
+const prism = $('.prism-container')
 const form = $('.form-send-api')
 const endp = $('.input-api')
-const div = $('.response')
+
+const main = () => {
+  let html_with_highlight = Prism.highlight(prism.innerText , Prism.languages.js, 'json'); 
+  prism.innerHTML = html_with_highlight;
+}
 
 const getResponseInfinityTrain = async (endpoint) => {
-  if ( endpoint === 'init' ){
-    const resp = await fetch(`${apiUrl}/test`)
-    const data = await resp.text()
-    console.log(data)
-    return data
-  }
 
-  const resp = await fetch(`${apiUrl}${endpoint}`)
-  const text = await resp.json()
+  const resp = await fetch(`${apiUrl}${endpoint}?pretty`)
+  let text = await resp.text()
+  if (!text.includes('\n')) text = JSON.stringify(JSON.parse(text), null, 2)
   return text
 }
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault()
   
-  if ( div.firstChild ){
-    div.removeChild(div.firstChild)
-  }
+  prism.firstChild.innerHTML = ""
   
   const json = await getResponseInfinityTrain(endp.value)
   
-  const formatter = new JSONFormatter(json, 3,{
-    theme: 'white'
-  });
-  div.appendChild(formatter.render());
+  let html_with_highlight = Prism.highlight(json , Prism.languages.js, 'json'); 
+  prism.innerHTML = html_with_highlight;
+
 })
 
-const initialData = await getResponseInfinityTrain('init') 
-const formatter = new JSONFormatter(initialData, 3,{
-  theme: 'white'
-});
-div.appendChild(formatter.render());
+main()
 
 export {
   $,
   apiUrl,
-  endp
+  endp,
+  getResponseInfinityTrain
 }
